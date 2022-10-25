6ee514b408907f14d112a2e88f7a7f2409193054
Adiba_Boufeldja <boufeldja.adiba@ifde-learning.fr>
2022-10-25 09:12:46 +0100

    Profile

---
+++ src/Form/ProfilType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Profil;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('image',FileType::class,[
+                'label'=> 'photo de profil',
+                'multiple' => false,
+                'mapped'=> false,
+                'required'=> false,
+                
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Profil::class,
+        ]);
+    }
+}
