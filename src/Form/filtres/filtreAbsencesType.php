9299987d8a63fd3d94a12810ef6faf39f721d412
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-11-18 13:41:45 +0100

    final8

--- src/Form/filtres/filtreAbsencesType.php
+++ src/Form/filtres/filtreAbsencesType.php
@@ -27,34 +27,72 @@ class FiltreAbsencesType extends AbstractType
     
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        
-        $builder
-        ->add('search', TextType::class, [
-            'attr' => array(
-                'placeholder' => 'Mot clé ou n° de…'
-            ),
-            'label' => false ,
-            'required' => false
-        ])
+        if(in_array('ROLE_INTERVENANT', $this->tokenStorage->getToken()->getUser()->getRoles())){
+            $builder
+            ->add('search', TextType::class, [
+                'attr' => array(
+                    'placeholder' => 'Mot clé ou n° de…'
+                ),
+                'label' => false ,
+                'required' => false
+            ])
+    
+    
+          
+            ->add('classe', EntityType::class, [
+                'class' => Classes::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
 
+                    ->innerJoin('u.modules', 'm')
 
+                    ->andWhere('m.classes = :user')
+
+                    ->setParameter('user',$this->tokenStorage->getToken()->getUser()->getClasse())
+                        ->orderBy('u.nom', 'ASC');
+                },
+                'expanded' => false,
+                'multiple' => false,
+                'choice_label' => 'nom',
+                'empty_data'=>'',
+                'required' => true,
+                'label'=>false,
+                'placeholder'=>'',
+         
+            ])
+    
+    
+            ;
+        }else{
+            $builder
+            ->add('search', TextType::class, [
+                'attr' => array(
+                    'placeholder' => 'Mot clé ou n° de…'
+                ),
+                'label' => false ,
+                'required' => false
+            ])
+    
+    
+          
+            ->add('classe', EntityType::class, [
+                'class' => Classes::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.nom', 'ASC');
+                },
+                'choice_label' => 'nom',
+                'label'=>false,
+                'empty_data'=>'',
+                'multiple' => false,
+                'required' => false
+            ])
+    
+    
+    
+            ;
+        }
       
-        ->add('classe', EntityType::class, [
-            'class' => Classes::class,
-            'query_builder' => function (EntityRepository $er) {
-                return $er->createQueryBuilder('u')
-                    ->orderBy('u.nom', 'ASC');
-            },
-            'choice_label' => 'nom',
-            'label'=>false,
-            'empty_data'=>'',
-            'multiple' => false,
-            'required' => false
-        ])
-
-
-
-        ;
 
    
 
