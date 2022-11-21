f5f6f8435a983a45b8a48256a4b0b4fc32366add
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-11-21 13:58:25 +0100

    test3

--- src/Form/AbsintervenantsType.php
+++ src/Form/AbsintervenantsType.php
@@ -1,41 +1,61 @@
 <?php
 
 namespace App\Form;
-
-use App\Entity\Absintervenants;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Doctrine\ORM\EntityRepository;
+use App\Entity\Classes;
+use App\Entity\Blocs;
+use App\Entity\Modules;
+use App\Entity\Users;
+use App\Entity\Absences;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 class AbsintervenantsType extends AbstractType
 {
+    private $em;
+
+    private $tokenStorage;
+    public function __construct(TokenStorageInterface   $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('date')
-            ->add('created_at')
-            ->add('created_by')
-            ->add('du')
-            ->add('au')
-            ->add('absent')
-            ->add('dateabsence')
-            ->add('enretard')
-            ->add('dateretard')
-            ->add('present')
-            ->add('datepresence')
-            ->add('userid')
-            ->add('intervenant')
-            ->add('module')
-            ->add('classe')
-            ->add('user')
-            ->add('tableau')
-        ;
-    }
+        
+            $builder
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Absintervenants::class,
-        ]);
-    }
+            ->add('classe', EntityType::class, [
+                'class' => Classes::class,
+                'expanded' => false,
+                'multiple' => false,
+                'choice_label' => 'nom',
+                'empty_data'=>'',
+                'required'=>false,
+                'label'=>'Classe',
+            ])
+         
+            ->add('tableau', CollectionType::class, [
+                'entry_type' => TableauAbsencesType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'prototype' => true,
+      
+           
+                'by_reference' => false,
+                'label' => false
+                
+            ])
+               
+                
+            ;
+        
+   
+            }
+
+   
 }
