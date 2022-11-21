2d2c35c5283713766273f1045fb7f81d7e2bf586
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-11-21 14:15:44 +0100

    test7

--- src/Controller/AbsintervenantsController.php
+++ src/Controller/AbsintervenantsController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 #[Route('/absintervenants')]
 class AbsintervenantsController extends AbstractController
