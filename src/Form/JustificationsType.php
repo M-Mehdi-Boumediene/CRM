da04fe9da56a798325b6d50cca199ce68100576c
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-11-23 11:11:05 +0100

    Justifications

--- src/Form/JustificationsType.php
+++ src/Form/JustificationsType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Justifications;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class JustificationsType extends AbstractType
