64965e7df3b90c715b92ab421158d9b24594424b
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-11-23 11:07:39 +0100

    Justifications

--- src/Form/JustificationsType.php
+++ src/Form/JustificationsType.php
@@ -12,7 +12,7 @@ class JustificationsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+            ->add('message')
          
      
         ;
