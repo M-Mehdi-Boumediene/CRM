f397865c9df2c63caeb12e7e18de8c5f634b79c3
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-11-24 13:24:18 +0100

    abs intervenants

--- src/Controller/AbsintervenantsController.php
+++ src/Controller/AbsintervenantsController.php
@@ -126,7 +126,7 @@ class AbsintervenantsController extends AbstractController
                $etudiants = $tableau->get('intervenant')->getData();
                 
                $tableauabsences->addIntervenant($etudiants[0]);
-               $absence->setUserid($etudiants[0]);
+               $absence->setUserid($tableau->get('userid')->getData());
                
                $dateabsence =$tableau->get('dateabsence')->getData();
                $retard = $tableau->get('retard')->getData();
