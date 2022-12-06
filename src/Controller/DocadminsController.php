0c4c1e27112bc4e574484e55386de109c572e6f9
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-06 14:31:13 +0100

    modif

--- src/Controller/DocadminsController.php
+++ src/Controller/DocadminsController.php
@@ -32,12 +32,7 @@ class DocadminsController extends AbstractController
             ]);
         } 
         
-        if( $user->getRoles() == ["ROLE_INTERVENANT"])
-        { 
-            return $this->render('docadmins/index.html.twig', [
-                'docadmins' => $docadminsRepository->findAll(),
-            ]);
-        }    
+ 
     
     }
 
