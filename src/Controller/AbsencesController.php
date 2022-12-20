da99240040524454f7278240e21f24bb4cb634c5
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2022-12-20 13:31:35 +0100

    Changement de logic intervennant plusieurs classes et modules

--- src/Controller/AbsencesController.php
+++ src/Controller/AbsencesController.php
@@ -114,10 +114,11 @@ class AbsencesController extends AbstractController
                     
 
                 $etudiants = $tableau->get('etudiant')->getData();
+
                 $user = $em->getRepository(Users::class)->findOneBy(array('etudiants'=>$etudiants[0]));
                 
-                $absence->setUserid($user);
-                $absence->setUser($user);
+              
+                $absence->addEtudiant($etudiants[0]);
                 $tableauabsences->addEtudiant($etudiants[0]);
                 $dateabsence =$tableau->get('dateabsence')->getData();
                 $retard = $tableau->get('retard')->getData();
