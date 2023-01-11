301d114249ea0b546d40fe2b31360b1b961259d7
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2023-01-11 16:59:05 +0100

    update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -74,9 +74,39 @@ class MainController extends AbstractController
         $etudiant = $etudiantsRepository->findByUser($user);
 
         $tableaunotes = $TableauNotesRepository->paretudiant1($etudiant);
+        if( $user->getRoles() == ["ROLE_ETUDIANT"])
+        { 
 
+            $events = $TableauNotesRepository->paretudiant1($etudiant);
+            $rdvs = [];
+            $rdvs2 = [];
+            foreach ($events as $event){
 
-  
+        
+                foreach ($event->getNotes() as $notes){
+
+                    foreach ($notes->getTableau() as $tableau){
+                        $rdvs[] =
+                        
+                            $tableau->getNote1()
+            ;
+                        
+                    }   
+
+                    $rdvs2[] = 
+                        $notes->getModule()->getNom()
+                    
+        
+                    ;
+                    
+                }
+    
+            
+
+                $data = json_encode($rdvs);
+                $data2 = json_encode($rdvs2);
+        }
+    }
 
         foreach ($intervenant as $inter){
           $classe =  $inter->getClasses();
@@ -102,35 +132,7 @@ class MainController extends AbstractController
           
             ]);         }else{
 
-                $events = $TableauNotesRepository->paretudiant1($etudiant);
-                $rdvs = [];
-                $rdvs2 = [];
-                foreach ($events as $event){
-        
-              
-                    foreach ($event->getNotes() as $notes){
-        
-                        foreach ($notes->getTableau() as $tableau){
-                            $rdvs[] =
-                            
-                                $tableau->getNote1()
-                ;
-                              
-                        }   
-        
-                        $rdvs2[] = 
-                            $notes->getModule()->getNom()
-                         
-            
-                        ;
-                        
-                    }
-           
-                  
-        
-                    $data = json_encode($rdvs);
-                    $data2 = json_encode($rdvs2);
-                }
+                
                 return $this->render('main/index.html.twig', [
                     'controller_name' => 'MainController',
                     'etudiant' => $etudiant,
