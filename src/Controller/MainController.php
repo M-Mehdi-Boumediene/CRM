38476647805c84b9d59057e0e862e1a26bf70315
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2023-01-11 15:37:44 +0100

    update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -90,6 +90,7 @@ class MainController extends AbstractController
         
                 foreach ($event->getTableau() as $event){
                     $rdvs[] = [
+                        'name' => $event->getNote()->getModules()->getNom(),
                         'data' => [$event->getNote1(),$event->getNote2(),$event->getNote3()],
                     
         
