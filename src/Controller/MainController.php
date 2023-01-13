5c70434b64bc743bc329a5d10e4c73cab1f21d42
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2023-01-13 10:45:47 +0100

    update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -76,11 +76,40 @@ class MainController extends AbstractController
         $tableaunotes = $TableauNotesRepository->paretudiant1($etudiant);
 
 
+        $events = $TableauNotesRepository->paretudiant1($etudiant);
+        $rdvs = [];
+        $rdvs2 = [];
+        foreach ($events as $event){
+
+      
+            foreach ($event->getNotes() as $notes){
+
+                foreach ($notes->getTableau() as $tableau){
+                    $rdvs[] =
+                    
+                        $tableau->getNote1()
+        ;
+                      
+                }   
+
+                $rdvs2[] = 
+                    $notes->getModule()->getNom();
+                 
+    
+                ;
+                
+            }
+   
+          
+
+            $data = json_encode($rdvs);
+            $data2 = json_encode($rdvs2);
+        }
 
         foreach ($intervenant as $inter){
           $classe =  $inter->getClasses();
         }
-        if( $user->getRoles() == ["ROLE_ENTREPRISE","ROLE_ADMIN"])
+        if( $user->getRoles() == ["ROLE_ENTREPRISE"] OR user->getRoles() == ["ROLE_ADMIN"] )
         { 
             return $this->render('main/index.html.twig', [
                 'controller_name' => 'MainController',
