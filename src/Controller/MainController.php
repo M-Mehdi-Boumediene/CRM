77ef88cbffb8395e3b5b4703839d93d566afb591
Mehdi Boumediene <mehdi.boumediene@ifde-learning.fr>
2023-01-13 11:20:07 +0100

    update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -73,8 +73,8 @@ class MainController extends AbstractController
         $classe = $user->getClasse();
         $etudiant = $etudiantsRepository->findByUser($user);
 
-        $tableaunotes = $TableauNotesRepository->paretudiant1(1);
-        $events = $TableauNotesRepository->paretudiant1(1);
+        $tableaunotes = $TableauNotesRepository->paretudiant1($etudiant);
+        $events = $TableauNotesRepository->paretudiant1($etudiant);
         $rdvs = [];
         $rdvs2 = [];
         foreach ($events as $event){
